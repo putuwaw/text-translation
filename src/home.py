89ee30911e33032190bcc949c276757c198bf01a
Putu Widyantara Artanta Wibawa <putuwaw973@gmail.com>
2024-11-13 15:47:05 +0000

    feat: add dynamic lang and text display

--- src/home.py
+++ src/home.py
@@ -1,34 +1,62 @@
-import streamlit as st
+import time
+import random
 from src.translator import translate
+import streamlit as st
+from st_copy_to_clipboard import st_copy_to_clipboard
+
 
 st.set_page_config(
     layout="wide",
     initial_sidebar_state="expanded",
 )
-
 st.header("Text Translation")
 
-col_origin, col_translate = st.columns(2)
+if "translation_result" not in st.session_state:
+    st.session_state["translation_result"] = None
 
+TEXT_CARET = "\u258c"
+lang_list = ["Indonesia", "English"]
+
+col_origin, col_translate = st.columns(2)
 with col_origin:
     lang_origin = st.selectbox(
         "Text",
-        ("Indonesia"),
+        lang_list,
     )
     txt = st.text_area("Text", value=None, height=200, label_visibility="collapsed")
+    target_languages = [lang for lang in lang_list if lang != lang_origin]
 
 with col_translate:
     lang_translate = st.selectbox(
         "Translate",
-        ("English"),
-        disabled=True,
+        target_languages,
     )
 
+    text_area_kwargs = {
+        "height": 200,
+        "disabled": True,
+        "label_visibility": "collapsed",
+    }
+
+    placeholder = st.empty()
+    placeholder.text_area("Translate", value=None, **text_area_kwargs)
+
     translated_text = translate(txt)
-    txt_translate = st.text_area(
-        "Translate",
-        value=translated_text,
-        height=200,
-        disabled=True,
-        label_visibility="collapsed",
-    )
+    if translated_text:
+        translated_display = ""
+        for word in translated_text.split():
+            translated_display += word + " "
+            placeholder.text_area(
+                "Translate",
+                value=translated_display.strip() + TEXT_CARET,
+                **text_area_kwargs
+            )
+            time.sleep(random.uniform(0, 0.3))
+
+        st.session_state["translation_result"] = translated_display.strip()
+
+        placeholder.text_area(
+            "Translate", value=translated_display.strip(), **text_area_kwargs
+        )
+
+        st_copy_to_clipboard(st.session_state["translation_result"])
