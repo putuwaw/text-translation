48c330dccd68518a02cf0c61e2b4d08dd3f2312d
Putu Widyantara Artanta Wibawa <putuwaw973@gmail.com>
2024-11-14 02:32:13 +0000

    feat: add upload file and language detection

--- src/home.py
+++ src/home.py
@@ -1,6 +1,8 @@
 import time
 import random
-from src.translator import translate
+from io import StringIO
+
+from src.translator import translate, verify_lang
 import streamlit as st
 from st_copy_to_clipboard import st_copy_to_clipboard
 
@@ -15,16 +17,37 @@ if "translation_result" not in st.session_state:
     st.session_state["translation_result"] = None
 
 TEXT_CARET = "\u258c"
-lang_list = ["Indonesia", "English"]
+lang_dict = {
+    "Indonesian": "id",
+    "English": "en",
+}
+
+warning_placeholder = st.empty()
+string_data = None
+uploaded_file = st.file_uploader("Choose a file", type=["txt", "csv", "md"])
+if uploaded_file is not None:
+    # To read file as bytes:
+    bytes_data = uploaded_file.getvalue()
+
+    # To convert to a string based IO:
+    stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
+
+    # To read file as string:
+    string_data = stringio.read()
 
 col_origin, col_translate = st.columns(2)
 with col_origin:
     lang_origin = st.selectbox(
         "Text",
-        lang_list,
+        list(lang_dict),
     )
-    txt = st.text_area("Text", value=None, height=200, label_visibility="collapsed")
-    target_languages = [lang for lang in lang_list if lang != lang_origin]
+    if uploaded_file and string_data:
+        txt = st.text_area(
+            "Text", value=string_data, height=200, label_visibility="collapsed"
+        )
+    else:
+        txt = st.text_area("Text", value=None, height=200, label_visibility="collapsed")
+    target_languages = [lang for lang in list(lang_dict) if lang != lang_origin]
 
 with col_translate:
     lang_translate = st.selectbox(
@@ -41,17 +64,27 @@ with col_translate:
     placeholder = st.empty()
     placeholder.text_area("Translate", value=None, **text_area_kwargs)
 
+    if txt:
+        if not verify_lang(txt, lang_dict[lang_origin]):
+            warning_placeholder.warning(
+                f"Please input {lang_origin} language!", icon=":material/warning:"
+            )
+            st.stop()
+        else:
+            warning_placeholder.empty()
+
     translated_text = translate(txt)
     if translated_text:
         translated_display = ""
-        for word in translated_text.split():
+        for idx, word in enumerate(translated_text.split()):
             translated_display += word + " "
             placeholder.text_area(
                 "Translate",
                 value=translated_display.strip() + TEXT_CARET,
-                **text_area_kwargs
+                **text_area_kwargs,
             )
-            time.sleep(random.uniform(0, 0.3))
+            if idx < 10:
+                time.sleep(random.uniform(0, 0.3))
 
         st.session_state["translation_result"] = translated_display.strip()
 
